ecd721e73b8c3fefbd99994e27515ebbc972c96b
le-christine <christine.le244@gmail.com>
2019-09-09 17:13:11 -0400

    added post request

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
 
 console.log("hello world");
-
-function signUp() {
+let token;
+function displaySignUp() {
   document.querySelector('.signup').addEventListener('click', function() {
     document.querySelector('#signUpForm').style.display = 'block';
   })
 }
 
-signUp();
+displaySignUp();
+
+function userSignUp() {
+  document.querySelector('#signUpSubmit').addEventListener('click', function() {
+    let email = document.querySelector('#signUpEmail').value;
+      let username = document.querySelector('#signUpUsername').value;
+    let password = document.querySelector('#signUpPassword').value;
+    postData(email, username, password);
+  })
+}
+
+
+let data;
+function postData(email, username, password) {
+  fetch('http://thesi.generalassemb.ly:8080/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({
+      email : `${email}`,
+      username: `${username}`,
+      password : `${password}`
+    })
+  })
+  .then((res) => {
+    console.log(res);
+    data = res;
+    // res.json().then(function(result) {
+    //   token = result.token;
+    // });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
