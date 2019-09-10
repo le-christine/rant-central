ea7229dcb77545dc09dc71025b46b7d851e715af
EngineerMel <ramelhaines25@gmail.com>
2019-09-10 15:01:52 -0400

    linked pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-
+//
 console.log("hello world");
+
 function displaySignUp() {
   document.querySelector('.signup').addEventListener('click', function() {
     document.querySelector('#signUpForm').style.display = 'block';
   })
 }
 
+function displayLoginIn() {
+  document.querySelector('.loginRant').addEventListener('click', function() {
+    document.querySelector('.loginForm').style.display = 'block';
+  })
+}
 displaySignUp();
 
 function userSignUp() {
@@ -14,14 +20,14 @@ function userSignUp() {
     let email = document.querySelector('#signUpEmail').value;
     let username = document.querySelector('#signUpUsername').value;
     let password = document.querySelector('#signUpPassword').value;
-    postData(email, username, password);
+    userSignUp(email, username, password);
   });
 }
 
 userSignUp();
 
 let token;
-function postData(email, username, password) {
+function userSignUp(email, username, password) {
   fetch('http://thesi.generalassemb.ly:8080/signup', {
     method: 'POST',
     headers: {
@@ -43,3 +49,52 @@ function postData(email, username, password) {
     console.log(error);
   })
 }
+
+
+
+
+
+// login to rant
+
+function displayLoginIn() {
+  document.querySelector('.loginRant').addEventListener('click', function() {
+    document.querySelector('.loginForm').style.display = 'block';
+  })
+}
+
+displayLoginIn();
+
+function loginUser() {
+  document.querySelector(".rantAway").addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('test')
+    let email = document.querySelector('#loginEmail').value;
+    let password = document.querySelector('#loginPassword').value;
+    console.log(email, password)
+    postData(email, password);
+  });
+  }
+
+loginUser();
+
+
+function postData(email, password) {
+  fetch('http://thesi.generalassemb.ly:8080/login', {
+  method: 'Post',
+  headers: {
+    'Content-type':'application/json'
+  },
+  body:JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+        .then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          location.href="main.html";
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      }
