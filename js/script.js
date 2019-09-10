110e9bb7d92f294a75c8eab4f1b83dcf9d67ea12
le-christine <christine.le244@gmail.com>
2019-09-10 15:02:07 -0400

    successul sign up leads to automatic sign in and page redirection

--- js/script.js
+++ js/script.js
@@ -1,110 +1,163 @@
-
 console.log("hello world");
-function displaySignUp() {
-  document.querySelector('.signup').addEventListener('click', function() {
-    document.querySelector('#signUpForm').style.display = 'block';
-  })
+
+// When signup button is clicked, form appears
+function displaySignUp(event) {
+  event.preventDefault();
+  document.querySelector('#signUpForm').style.display = 'block';
 }
-
-// displaySignUp();
-
-function userSignUp() {
-  document.querySelector('.signUpSubmit').addEventListener('click', function(e) {
-    e.preventDefault();
-    let email = document.querySelector('#signUpEmail').value;
-    let username = document.querySelector('#signUpUsername').value;
-    let password = document.querySelector('#signUpPassword').value;
-    postData(email, username, password);
-  });
-}
-
-// userSignUp();
 
 // Signup POST Request
-let token;
-function postData(email, username, password) {
+// after successful request, redirects user to new page
+function postData(event) {
+  event.preventDefault();
+  let email = document.querySelector('#signUpEmail');
+  let username = document.querySelector('#signUpUsername');
+  localStorage.setItem('username', username.value);
+  let password = document.querySelector('#signUpPassword');
+
   fetch('http://thesi.generalassemb.ly:8080/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': "application/json"
-    },
-    body: JSON.stringify({
-      email : `${email}`,
-      username: `${username}`,
-      password : `${password}`
-    })
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              email: email.value,
+              password: password.value,
+              username: username.value
+          })
   })
-  .then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);
-    token = data["token"];
+  .then((res) => {
+      return res.json();
   })
-  .catch((error) => {
-    console.log(error);
+  .then((res) => {
+      localStorage.setItem('user', res.token);
   })
+  .catch((err) => {
+      console.log(err);
+  })
+  window.location.href="./main.html";
 }
 
+// after successul sign up, changes username element
+function changeUser() {
+  document.querySelector('.loggedInUser').innerText = localStorage.getItem('username');
+};
+
+function updateDom() {
+    fetch("http://thesi.generalassemb.ly:8080/user/post", {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user')
+        }
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        const list = document.querySelector('.posts');
+
+        for (let i = 0; i < res.length; i++) {
+            const item = document.createElement('li');
+            const title = document.createElement('h3');
+            const description = document.createElement('p');
+            item.appendChild(title);
+            item.appendChild(description);
+            title.innerText = res[i].title;
+            description.innerText = res[i].description;
+            list.appendChild(item);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+function createPost(event) {
+    event.preventDefault();
+    const title = document.querySelector('.title');
+    const description = document.querySelector('.description');
+
+    fetch("http://thesi.generalassemb.ly:8080/post", {
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJleHByZXNhc2Rmc0B0cmFpbi5jb20iLCJleHAiOjE1NjgxNDkwMDAsImlhdCI6MTU2ODEzMTAwMH0.28OoEKc3FBXn5iqg1d1C_sjuowklVuAPAs8RYAnPXjMoNFuebNRCQJJiU-Dp6PQKhzgNES95ftckHhw9aTNyFg",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title.value,
+            description: description.value
+        })
+    })
+    .then((res) => {
+        console.log(res);
+        updateDom(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
 
 // Load all posts and populate document
 function listAllPosts() {
   fetch ('http://thesi.generalassemb.ly:8080/post/list', {
     method: 'GET'
   })
-  .then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);
+  .then(function(result) {
+  console.log(result.json());
   })
 };
 
-listAllPosts();
 
-function populatePosts(title, content, user) {
+function populatePosts(data) {
+  let title, content, username;
+
+  for (let i = 0; i<data.length; i++) {
+    title = data[i]['title'];
+    username = data[i]['user']['username'];
+    if (data[i]['description']) {
+      content = data[i]['description'];
+    } else {
+      console.log(data[i]['description']);
+      content = ' ';
+    }
+  //manipulateDomPosts(title, content, username)
+  }
+};
+
+
+function manipulateDomPosts(title, content, user) {
   // creates a div for each post and appends it to posts-container
   let userPost = document.createElement('div');
   userPost.classList = 'userPost';
   document.querySelector(".posts-container").appendChild(userPost);
 
-  // a user's post
-  let newPost = document.querySelector('#userPost');
 
   //creates a span for post title and appends it to the post
   let postOwner = document.createElement('span');
   postOwner.classList = 'postOwner';
   postOwner.innerText = `Posted by: ${user}`;
+  //console.log(postOwner);
 
   // creates a post title and appends it to the post
   let postTitle = document.createElement('h3');
   postTitle.classList = 'userPostTitle';
   postTitle.innerText = title;
-  newPost.appendChild(postTitle);
-
+  //console.log(postTitle);
   // creates a div with post's contents and appends it to the post
   let postContent = document.createElement('p');
   postContent.classList = 'userPostContent';
   postContent.innerText = content;
-  newPost.appendChild(postContent);
-
+  //console.log(postContent);
   // creates a comment box and appends it to the post
   let commentsBox = document.createElement('div');
   commentsBox.classList = "userPostComments";
-  newPost.appendChild(commentsBox);
-
-  // let titlePost = document.querySelector('.userPostTitle');
-  // let postContent = document.querySelector('.userPostContent');
-  // let postOwner = document.querySelector('postOwner');
-
+  //console.log(commentsBox);
+  // a user's post
+  let newPost = document.querySelector('#userPost');
+  newPost.append(postOwner);
+  newPost.append(postContent);
+  newPost.append(commentsBox);
 };
 
-/*
-<p>Posted by: <span class="userPost"></span></p>
-<h3 class = "userPostTitle">Today the MTA made me late by 30 minutes</h3>
-<p class = "userPostContent">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<div class = "userPostComents">
- */
-
-// Allow a user to create and delete their own posts.
 // Allow a user to view comments on other posts.
 // Allow a user to create and delete their own comments.
 // Allow a user to update their profile information.
